1183f71da25bf7db35705eabecfee5f93f796b81
crpto genesis <prishankifficial@gmail.com>
2024-01-27 01:07:49 +0530

    git show

--- notes.js
+++ notes.js
@@ -1,2 +1,3 @@
 // git reset is used to undo git add . action  or a specific file
 // git reset --hard will undo all the changes made to everyfile untill previous commit
+// to see file changes after commit you can type git show <commit head> --name-only
