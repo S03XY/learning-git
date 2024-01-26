4d79c597119aabfd6ec06100f3949bfe7c82bd27
crpto genesis <prishankifficial@gmail.com>
2024-01-27 01:39:28 +0530

    added notes about git rm --cached

--- notes.js
+++ notes.js
@@ -1,3 +1,4 @@
 // git reset is used to undo git add . action  or a specific file
-// git reset --hard will undo all the changes made to everyfile untill previous commit
-// to see file changes after commit you can type git show <commit head> --name-only
+// git reset --hard will undo all the changes made to everyfile untill previous commit 
+// to see file changes after commit you can type git show <commit head> --name-only for soft reset use git reset --soft
+// to remove a committed file add it in gitignore then git rm --cached <file> then git add . git commit and git push
